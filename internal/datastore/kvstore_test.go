3ed4bd472cb3c9427edcf0cedffdec78d84c0e6e
Jeff Cuevas-Koch <jcuevas-koch@gmvsync.com>
2020-05-05 19:35:41 -0700

    (+semver: feature) Add TTL to SetValuesRequest
    Signed-off-by: Jeff Cuevas-Koch <jcuevas-koch@gmvsync.com>

--- internal/datastore/kvstore_test.go
+++ internal/datastore/kvstore_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/golang/protobuf/ptypes"
 	"github.com/syncromatics/kvetch/internal/datastore"
 	apiv1 "github.com/syncromatics/kvetch/internal/protos/kvetch/api/v1"
 
@@ -119,6 +120,83 @@ func Test_Get(t *testing.T) {
 			},
 		},
 	})
+}
+
+func Test_TTL(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "Test_GetPrefix")
+	assert.NilError(t, err)
+
+	store, err := datastore.NewKVStore(tmpDir)
+	assert.NilError(t, err)
+
+	ttl := 2 * time.Second
+	err = store.Set(&apiv1.SetValuesRequest{
+		Messages: []*apiv1.KeyValue{
+			&apiv1.KeyValue{
+				Key:   "test/1/stuff",
+				Value: []byte("value 1"),
+			},
+			&apiv1.KeyValue{
+				Key:   "test/1/stuff2",
+				Value: []byte("value 2 longer"),
+			},
+			&apiv1.KeyValue{
+				Key:   "test/2/stuff",
+				Value: []byte("bad value"),
+			},
+		},
+		TtlDuration: ptypes.DurationProto(ttl),
+	})
+	assert.NilError(t, err)
+
+	values, err := store.Get(&apiv1.GetValuesRequest{
+		Requests: []*apiv1.GetValuesRequest_GetValue{
+			&apiv1.GetValuesRequest_GetValue{
+				Key: "test/1/stuff",
+			},
+			&apiv1.GetValuesRequest_GetValue{
+				Key: "test/1/stuff2",
+			},
+			&apiv1.GetValuesRequest_GetValue{
+				Key: "test/2/stuff",
+			},
+		},
+	})
+	assert.NilError(t, err)
+
+	assert.DeepEqual(t, values, &apiv1.GetValuesResponse{
+		Messages: []*apiv1.KeyValue{
+			&apiv1.KeyValue{
+				Key:   "test/1/stuff",
+				Value: []byte("value 1"),
+			},
+			&apiv1.KeyValue{
+				Key:   "test/1/stuff2",
+				Value: []byte("value 2 longer"),
+			},
+			&apiv1.KeyValue{
+				Key:   "test/2/stuff",
+				Value: []byte("bad value"),
+			},
+		},
+	})
+
+	time.Sleep(ttl)
+	values, err = store.Get(&apiv1.GetValuesRequest{
+		Requests: []*apiv1.GetValuesRequest_GetValue{
+			&apiv1.GetValuesRequest_GetValue{
+				Key: "test/1/stuff",
+			},
+			&apiv1.GetValuesRequest_GetValue{
+				Key: "test/1/stuff2",
+			},
+			&apiv1.GetValuesRequest_GetValue{
+				Key: "test/2/stuff",
+			},
+		},
+	})
+	assert.NilError(t, err)
+	assert.Equal(t, len(values.Messages), 0)
 }
 
 func Test_Subscribe(t *testing.T) {
