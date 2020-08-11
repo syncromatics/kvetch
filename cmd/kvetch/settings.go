861756515d506674535b4d57ecf9d165eee9b2e5
kgrant <kgrant@syncromatics.com>
2020-08-11 12:49:34 -0700

    (+semver:feature) add support for environmental variables to optionally configure KVStore (badger) for more aggressive compaction/garbage collection of value logs.

--- cmd/kvetch/settings.go
+++ cmd/kvetch/settings.go
@@ -6,6 +6,9 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/golang/protobuf/ptypes/wrappers"
+	kvstore "github.com/syncromatics/kvetch/internal/datastore"
 )
 
 type settings struct {
@@ -13,6 +16,81 @@ type settings struct {
 	PrometheusPort            int
 	Datastore                 string
 	GarbageCollectionInterval time.Duration
+	KVStoreOptions            *kvstore.KVStoreOptions
+}
+
+func getKVStoreOptions() (*kvstore.KVStoreOptions, error) {
+	allErrors := []string{}
+	kvStoreOptions := &kvstore.KVStoreOptions{}
+	enableTruncateString, ok := os.LookupEnv("ENABLE_TRUNCATE")
+	if ok {
+		enableTruncate, err := strconv.ParseBool(enableTruncateString)
+		if err != nil {
+			allErrors = append(allErrors, fmt.Sprintf("ENABLE_TRUNCATE is not a valid bool '%s'", enableTruncateString))
+		} else {
+			kvStoreOptions.EnableTruncate = &wrappers.BoolValue{Value: enableTruncate}
+		}
+	}
+	maxTableSizeString, ok := os.LookupEnv("MAX_TABLE_SIZE")
+	if ok {
+		maxTableSize, err := strconv.ParseInt(maxTableSizeString, 10, 64)
+		if err != nil {
+			allErrors = append(allErrors, fmt.Sprintf("MAX_TABLE_SIZE is not a valid int64 '%s'", maxTableSizeString))
+		} else {
+			kvStoreOptions.MaxTableSize = &wrappers.Int64Value{Value: maxTableSize}
+		}
+	}
+	levelOneSizeString, ok := os.LookupEnv("LEVEL_ONE_SIZE")
+	if ok {
+		levelOneSize, err := strconv.ParseInt(levelOneSizeString, 10, 64)
+		if err != nil {
+			allErrors = append(allErrors, fmt.Sprintf("LEVEL_ONE_SIZE is not a valid int64 '%s'", levelOneSizeString))
+		} else {
+			kvStoreOptions.LevelOneSize = &wrappers.Int64Value{Value: levelOneSize}
+		}
+	}
+	levelSizeMultiplierString, ok := os.LookupEnv("LEVEL_SIZE_MULTIPLIER")
+	if ok {
+		levelSizeMultiplier, err := strconv.ParseInt(levelSizeMultiplierString, 10, 32)
+		if err != nil {
+			allErrors = append(allErrors, fmt.Sprintf("LEVEL_SIZE_MULTIPLIER is not a valid int32 '%s'", levelSizeMultiplierString))
+		} else {
+			kvStoreOptions.LevelSizeMultiplier = &wrappers.Int32Value{Value: int32(levelSizeMultiplier)}
+		}
+	}
+	numberOfLevelZeroTablesString, ok := os.LookupEnv("NUMBER_OF_LEVEL_ZERO_TABLES")
+	if ok {
+		numberOfLevelZeroTables, err := strconv.ParseInt(numberOfLevelZeroTablesString, 10, 32)
+		if err != nil {
+			allErrors = append(allErrors, fmt.Sprintf("NUMBER_OF_LEVEL_ZERO_TABLES is not a valid int32 '%s'", numberOfLevelZeroTablesString))
+		} else {
+			kvStoreOptions.NumberOfLevelZeroTables = &wrappers.Int32Value{Value: int32(numberOfLevelZeroTables)}
+		}
+	}
+	numberOfLevelZeroTablesUntilForceCompactionString, ok := os.LookupEnv("NUMBER_OF_ZERO_LEVEL_TABLES_UNTIL_FORCE_COMPACTION")
+	if ok {
+		numberOfLevelZeroTablesUntilForceCompactaion, err := strconv.ParseInt(numberOfLevelZeroTablesUntilForceCompactionString, 10, 32)
+		if err != nil {
+			allErrors = append(allErrors, fmt.Sprintf("NUMBER_OF_ZERO_LEVEL_TABLES_UNTIL_FORCE_COMPACTION is not a valid int32 '%s'", numberOfLevelZeroTablesUntilForceCompactionString))
+		} else {
+			kvStoreOptions.NumberOfLevelZeroTablesUntilForceCompaction = &wrappers.Int32Value{Value: int32(numberOfLevelZeroTablesUntilForceCompactaion)}
+		}
+	}
+	garbageCollectionDiscardRatioString, ok := os.LookupEnv("GARBAGE_COLLECTION_DISCARD_RATIO")
+	if ok {
+		garbageCollectionDiscardRatio, err := strconv.ParseFloat(garbageCollectionDiscardRatioString, 32)
+		if err != nil {
+			allErrors = append(allErrors, fmt.Sprintf("GARBAGE_COLLECTION_DISCARD_RATIO is not a valid float32 '%s'", garbageCollectionDiscardRatioString))
+		} else {
+			kvStoreOptions.GarbageCollectionDiscardRatio = &wrappers.FloatValue{Value: float32(garbageCollectionDiscardRatio)}
+		}
+	}
+
+	if len(allErrors) > 0 {
+		return nil, fmt.Errorf("Failed configuring KVStore: %s", strings.Join(allErrors, ", "))
+	}
+
+	return kvStoreOptions, nil
 }
 
 func getSettingsFromEnv() (*settings, error) {
@@ -55,6 +133,11 @@ func getSettingsFromEnv() (*settings, error) {
 		}
 	}
 
+	kvStoreOptions, err := getKVStoreOptions()
+	if err != nil {
+		allErrors = append(allErrors, err.Error())
+	}
+
 	if len(allErrors) > 0 {
 		return nil, fmt.Errorf("Missing required environment variables: %s", strings.Join(allErrors, ", "))
 	}
@@ -64,5 +147,6 @@ func getSettingsFromEnv() (*settings, error) {
 		PrometheusPort:            prometheusPortInt,
 		Datastore:                 datastore,
 		GarbageCollectionInterval: duration,
+		KVStoreOptions:            kvStoreOptions,
 	}, nil
 }
